5c5f1080b3f39d6e2ab3385bfc1e3509b0dbcec8
Nick Hirakawa <nhirakawa@hubspot.com>
2017-04-02 15:32:50 -0400

    Changes to blink-led-array

--- blink-led-array/blink.c
+++ blink-led-array/blink.c
@@ -1,25 +1,86 @@
-/* Blinker Demo */
+#include "blink.h"
 
-#include <avr/io.h>
-#include <util/delay.h>
+int main(void)
+{
+  init_ddrb();
 
+  while (1)
+  {
+    for(int i = 0; i < ITERATIONS; i++)
+    {
+      knight_rider();
+    }
 
-int main(void) {
+    for(int i = 0; i < 2 * ITERATIONS; i++)
+    {
+      blink();
+    }
 
-  DDRD |= 0b11111111;            /* Data Direction Register B:
-                                    writing a one to the bit
-                                    enables output. */
-  while (1) {
-
-    int i;
-    for(i = 0; i < 255; i++){
-      writeInt(i);
-      _delay_ms(100);
+    for(int i = 0; i < ITERATIONS; i++)
+    {
+      criss_cross();
     }
   }
-  return (0);
 }
 
-void writeInt(int i){
-  PORTD = 0xFF & i;
+void blink()
+{
+  uint8_t off = 0xFF;
+  output_led_state(off);
+  _delay_ms(4 * DELAY);
+  uint8_t on = 0x00;
+  output_led_state(on);
+  _delay_ms(4 * DELAY);
+}
+
+void criss_cross()
+{
+  uint8_t pattern = 0xAA;
+  for(int i = 0; i < 8; i++)
+  {
+    pattern = ~pattern;
+    output_led_state(pattern);
+    _delay_ms(2 * DELAY);
+  }
+}
+
+void knight_rider()
+{
+  for (int i = 8; i >= 0; i--)
+  {
+    output_led_state(_BV(i));
+    _delay_ms(DELAY);
+  }
+  for (int i = 0; i < 8; i++)
+  {
+    output_led_state(_BV(i));
+    _delay_ms(DELAY);
+  }
+}
+
+void init_ddrb(void)
+{
+  DDRB = 0b00000111; //1 = output, 0 = input
+  PORTB = 0b00000000;
+}
+
+void output_led_state(uint8_t led_state)
+{
+  SH_CP_low();
+  ST_CP_low();
+  for (int i = 0; i < 8; i++)
+  {
+    if ((_BV(i) & led_state) != _BV(i))
+    {
+      DS_high();
+    }
+    else
+    {
+      DS_low();
+    }
+
+    SH_CP_high();
+    SH_CP_low();
+  }
+  ST_CP_high();
 }
---
+++ blink-led-array/blink.h
@@ -0,0 +1,33 @@
+
+#include <avr/io.h>
+#include <util/delay.h>
+#include <stdint.h>
+
+#define DS_PORT    PORTB
+#define DS_PIN     2
+#define ST_CP_PORT PORTB
+#define ST_CP_PIN  1
+#define SH_CP_PORT PORTB
+#define SH_CP_PIN  0
+ 
+#define DS_low()  DS_PORT&=~_BV(DS_PIN)
+#define DS_high() DS_PORT|=_BV(DS_PIN)
+#define ST_CP_low()  ST_CP_PORT&=~_BV(ST_CP_PIN)
+#define ST_CP_high() ST_CP_PORT|=_BV(ST_CP_PIN)
+#define SH_CP_low()  SH_CP_PORT&=~_BV(SH_CP_PIN)
+#define SH_CP_high() SH_CP_PORT|=_BV(SH_CP_PIN)
+
+#define DELAY 50
+#define ITERATIONS 5
+
+int main(void);
+
+void init_ddrb(void);
+
+void output_led_state(uint8_t led_state);
+
+void blink(void);
+
+void criss_cross(void);
+
+void knight_rider(void);
